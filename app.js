2485edfe6e28810c9febb78466ed74ce766d69a9
JoeZ <josephzasiebida@gmail.com>
2020-12-15 20:55:21 -0600

    'module-1'

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-fs
+const http = require('http')
+
+const fs = require('fs');
+
+console.log(routes.someText)
+
+const routes = require('./routes')
+
+const server = http.createServer(routes.handler)
+
+server.listen(3000)
